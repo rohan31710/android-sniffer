e6f558327c5efda88a12f8787f1fd22504c4b9e0
sam.smithreams@gmail.com <sam.smithreams@gmail.com@6c0aa805-eda2-f876-4f90-38be2705b40e>
2012-05-28 07:42:43 +0000

    Added dropbox support.

---
+++ trunk/ android-sniffer/AndroidSniffer/src/edu/droidshark/android/ui/fragments/activity/UploadFragment.java
@@ -0,0 +1,59 @@
+/**
+ * Created May 27, 2012
+ */
+package edu.droidshark.android.ui.fragments.activity;
+
+import java.io.File;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockDialogFragment;
+
+import edu.droidshark.android.ui.activities.DroidSharkActivity;
+
+/**
+ * Dialog fragment for selecting a file to upload
+ * 
+ * @author Sam SmithReams
+ * 
+ */
+public class UploadFragment extends SherlockDialogFragment
+{
+	private File path;
+
+	/**
+	 * @param path
+	 *            The path to look for files
+	 */
+	public UploadFragment(File path)
+	{
+		this.path = path;
+	}
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState)
+	{
+		final File[] files = path.listFiles();
+
+		final CharSequence[] items = new CharSequence[files.length];
+		for (int i = 0; i < files.length; i++)
+			items[i] = files[i].getName();
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		builder.setTitle("Select File");
+		builder.setItems(items, new DialogInterface.OnClickListener()
+		{
+			public void onClick(DialogInterface dialog, int item)
+			{
+				DroidSharkActivity dsActivity = (DroidSharkActivity) UploadFragment.this
+						.getActivity();
+				dsActivity.uploadCaptureFile(files[item]);
+			}
+		});
+
+		return builder.create();
+	}
+}
--- trunk/ android-sniffer/AndroidSniffer/src/edu/droidshark/constants/SnifferConstants.java
+++ trunk/ android-sniffer/AndroidSniffer/src/edu/droidshark/constants/SnifferConstants.java
@@ -1,9 +1,16 @@
 package edu.droidshark.constants;
 
+import com.dropbox.client2.session.Session.AccessType;
+
 public interface SnifferConstants
 {
 	public static final boolean DEBUG = true; // Enable debug Log messages
 	public static final int SNIFFERPANE = 0x01, PACKETVIEWPANE = 0x04;
 	public static final String TCPDUMP_OPTIONS = "TCPDUMP_OPTIONS";
+	public static final String DROPBOX_APP_KEY = "8h9vli5kaemkqwa";
+	public static final String DROPBOX_APP_SECRET = "jaspssmucidhxu7";
+	public static final AccessType ACCESS_TYPE = AccessType.APP_FOLDER;
+	public static final String DROPBOX_KEY_NAME = "DROPBOX_KEY";
+	public static final String DROPBOX_SECRET_NAME = "DROPBOX_SECRET";
 	//NOWPLAYINGPANE = 0x08;
 }
